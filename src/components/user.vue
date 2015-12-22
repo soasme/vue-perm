752a1cfdb59249fbf65ee8f3a472654653681914
soasme <soasme@gmail.com>
2015-12-22 18:04:08 +0800

    Init user component.

---
+++ src/components/user.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="user">
+    <div class="avatar">
+      <img src="{{ avatar }}" v-if="avatar">
+    </div>
+    <p class="nickname">
+      {{ nickname }}
+    </p>
+    <slot name="action"></slot>
+  </div>
+</template>
+
+<style lang="stylus">
+.user
+  width: 100px
+  overflow: hidden
+
+  .avatar
+    text-align: center
+
+    img
+      max-width: 50px
+      max-height: 50px
+      border-radius: 50px
+      border: solid 1px black
+
+  .nickname
+    margin: 0 auto
+    text-align: center
+
+  slot[name=action]
+    margin: 0 auto
+    text-align: center
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      avatar: "http://img6.douban.com/icon/u3770153-22.jpg",
+      nickname: "soasme"
+    }
+  }
+}
+</script>
