efc7ff449ea53d4b902ddeb4bf3c2775a21a45c3
soasme <soasme@gmail.com>
2015-12-23 15:36:22 +0800

    Define AuthorizePermissionToUserGroup component.

---
+++ src/components/authorize-permission-to-user-group.vue
@@ -0,0 +1,71 @@
+<template>
+  <ul>
+    <li v-for="userGroup in userGroups">
+      <label>
+        <input type="checkbox"
+          value="{{ userGroup.id }}"
+          v-model="authorizedUserGroupIDs"
+          @click="togglePermissionToUserGroup(permission, userGroup)">
+        {{ userGroup.title }}
+      </label>
+    </li>
+  </ul>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+
+export default {
+
+  props: ['userGroups', 'permission'],
+
+  data () {
+    return {
+      authorizedUserGroupIDs: []
+    }
+  },
+
+  ready () {
+    this.$http.get(
+      `/perm/permissions/${ this.permission.id }/user_groups`
+    ).then((resp) => {
+      this.$set('authorizedUserGroupIDs', resp.data.data.user_groups.map((group) => {
+        return group.id
+      }))
+    }, (resp) => {
+      console.error('load user group data failed', this.permission, this.userGroups)
+    })
+  },
+
+  methods: {
+    togglePermissionToUserGroup (permission, userGroup) {
+      if (this.authorizedUserGroupIDs.indexOf(userGroup.id) !== -1) {
+        this.$http.delete(
+          `/perm/permissions/${ permission.id }/user_groups/${ userGroup.id }`
+        ).then((resp) => {
+          this.$dispatch('user-group-permission-authorized', {
+            permission: permission,
+            userGroup, userGroup
+          }, (resp) => {
+            alert('Cancel failed')
+          })
+        })
+      } else {
+        this.$http.put(
+          `/perm/permissions/${ permission.id }/user_groups/${ userGroup.id }`
+        ).then((resp) => {
+          this.$dispatch('user-group-permission-authorized', {
+            permission: permission,
+            userGroup, userGroup
+          })
+        }, (resp) => {
+          alert('Auth failed')
+        })
+      }
+    }
+  }
+}
+</script>
