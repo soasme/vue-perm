0df96f2cc82411934c8ae7e97de585fc3a1427ab
soasme <soasme@gmail.com>
2015-12-23 10:50:30 +0800

    Define permission list component.

---
+++ src/components/permission-list.vue
@@ -0,0 +1,44 @@
+<template>
+  <add-permission @permission-created="appendNewPermission"></add-permission>
+  <div v-for="permission in permissions">
+    <permission :permission="permission" ></permission>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import Permission from './permission.vue'
+import AddPermission from './add-permission.vue'
+import User from './user.vue'
+
+Vue.use(VueResource)
+
+export default {
+  data () {
+    return {
+      permissions: []
+    }
+  },
+
+  components: {
+    Permission,
+    AddPermission,
+  },
+
+  ready () {
+    this.$http.get('/perm/permissions').then((resp) => {
+      this.$set('permissions', resp.data.data.permissions)
+    })
+  },
+
+  methods: {
+    appendNewPermission: function (permission) {
+      this.permissions.splice(0, 0, permission)
+    }
+  }
+
+
+}
+</script>
+
