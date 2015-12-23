e6b6cde627beb6392a027c98f25b512a364f3855
soasme <soasme@gmail.com>
2015-12-23 10:51:23 +0800

    Remove dummy code.

--- src/app.vue
+++ src/app.vue
@@ -1,42 +1,24 @@
-<template lang="jade">
-div
-  img(class="logo", src="./assets/logo.png")
-  h1 {{msg}}
-  comp-a
-  comp-b
-  counter
-  search-bar
+<template>
+ <h1>{{ msg }}</h1>
+ <permission-list></permission-list>
 </template>
 
 <script>
-import CompA from './components/a.vue'
-import CompB from './components/b.vue'
-import Counter from './components/counter.vue'
-import SearchBar from './components/search-bar.vue'
+import User from './components/user.vue'
+import PermissionList from './components/permission-list.vue'
 
 export default {
   data () {
     return {
-      msg: 'Hello from vue-loader!'
+      msg: 'Permission Manager'
     }
   },
   components: {
-    CompA,
-    CompB,
-    Counter,
-    SearchBar,
+    User,
+    PermissionList,
   }
 }
 </script>
 
 <style lang="stylus">
-font-stack = Helvetica, sans-serif
-primary-color = #999
-body
-  font 100% font-stack
-  color primary-color
-
-.logo
-  width 40px
-  height 40px
 </style>
